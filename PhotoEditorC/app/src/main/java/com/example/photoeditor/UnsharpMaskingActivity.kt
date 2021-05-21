c85c32b9fd3156b5823ac323331c4ff737c114de
DESKTOP-6KR7LVK\Пользователь <lyana688@gmail.com>
2021-05-21 21:18:15 +0700

    Исправлено маскирование

--- PhotoEditorC/app/src/main/java/com/example/photoeditor/UnsharpMaskingActivity.kt
+++ PhotoEditorC/app/src/main/java/com/example/photoeditor/UnsharpMaskingActivity.kt
@@ -8,56 +8,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.widget.*
+import com.example.photoeditor.databinding.ActivityChooseBinding
+import com.example.photoeditor.databinding.ActivityUnsharpMaskingBinding
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 class UnsharpMaskingActivity : AppCompatActivity() {
-    private lateinit var imageUri: Uri
     private lateinit var finalBitmap: Bitmap
     private lateinit var bitmapBefore: Bitmap
     private lateinit var bitmap: Bitmap
     private lateinit var imageView: ImageView
+    private lateinit var binding: ActivityUnsharpMaskingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_unsharp_masking)
+        binding = ActivityUnsharpMaskingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         init()
     }
 
     private fun init() {
 
-        val changeButton = findViewById<Button>(R.id.changeButton)
-        val amountEditText = findViewById<EditText>(R.id.amountEditText)
-        val radiusEditText = findViewById<EditText>(R.id.radiusEditText)
-        val thresholdEditText = findViewById<EditText>(R.id.thresholdEditText)
-        val returnBackButton = findViewById<ImageButton>(R.id.returnBackButton)
-        val backButton = findViewById<Button>(R.id.backButton)
-        val applyButton = findViewById<Button>(R.id.applyButton)
-
         val uriStr = intent.getStringExtra("imgUri")
         val uri = Uri.parse(uriStr)
-        imageView = findViewById(R.id.imageViewEdit)
-        imageView.setImageURI(uri)
+        binding.imageViewEdit.setImageURI(uri)
 
-        val drawable = imageView.drawable as BitmapDrawable
+        val drawable = binding.imageViewEdit.drawable as BitmapDrawable
         bitmap = drawable.bitmap
         bitmapBefore = bitmap
         finalBitmap = bitmap
 
         // кнопка запуска алгоритма
-        changeButton.setOnClickListener {
-            val amount = amountEditText.text.toString().toDoubleOrNull()
-            val radius = radiusEditText.text.toString().toDoubleOrNull()
-            val threshold = thresholdEditText.text.toString().toIntOrNull()
+        binding.changeButton.setOnClickListener {
+            val amount = binding.amountEditText.text.toString().toDoubleOrNull()
+            val radius = binding.radiusEditText.text.toString().toDoubleOrNull()
+            val threshold = binding.thresholdEditText.text.toString().toIntOrNull()
             when {
                 amount == null || radius == null || threshold == null -> {
                     Toast.makeText(this, "Введите корректные данные", Toast.LENGTH_SHORT).show()
                 }
-                amount < 0 || amount > 1 || threshold <= 0 || radius <= 0 -> {
+                threshold <= 0 || threshold <= 0 || radius <= 0 -> {
                     Toast.makeText(this, "Введите корректные данные", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
@@ -68,20 +61,20 @@ class UnsharpMaskingActivity : AppCompatActivity() {
         }
 
         // кнопка отмены изменений
-        returnBackButton.setOnClickListener {
+        binding.returnBackButton.setOnClickListener {
             imageView.setImageBitmap(bitmapBefore)
             finalBitmap = bitmapBefore
         }
 
         // кнопка возвращения без сохранения
-        backButton.setOnClickListener {
+        binding.backButton.setOnClickListener {
             val editIntent = Intent(this, ChooseActivity::class.java)
             editIntent.putExtra("imgUri", uri.toString())
             startActivity(editIntent)
         }
 
         // сохранения и перехода на другую страницу
-        applyButton.setOnClickListener {
+        binding.applyButton.setOnClickListener {
             val outFile = createImageFile()
             try {
                 FileOutputStream(outFile).use { out ->
@@ -113,7 +106,7 @@ class UnsharpMaskingActivity : AppCompatActivity() {
         maskCreation(firstPixels, blurPixels, maskPixels)
         maskApplication(firstPixels, maskPixels, resultPixels, amount, threshold)
 
-        imageMask.setPixels(resultPixels, 0, width, 0, 0, width, height);
+        imageMask.setPixels(resultPixels, 0, width, 0, 0, width, height)
         imageView.setImageBitmap(imageMask)
         bitmapBefore = finalBitmap
         finalBitmap = imageMask
@@ -142,9 +135,9 @@ class UnsharpMaskingActivity : AppCompatActivity() {
             for (j in 0..radius.toInt()) {
                 // размытие строки
                 if (i + j < numberString * width) {
-                    r += (first[i + j] and 0x00F0000 shr 16)
-                    g += (first[i + j] and 0x0000F00 shr 8)
-                    b += (first[i + j] and 0x000000F)
+                    r += (first[i + j] and 0x00FF0000 shr 16)
+                    g += (first[i + j] and 0x0000FF00 shr 8)
+                    b += (first[i + j] and 0x000000FF)
                     numberRGB += 1
                 }
 
@@ -187,41 +180,24 @@ class UnsharpMaskingActivity : AppCompatActivity() {
     // функция создания маски
     private fun maskCreation(first: IntArray, blur: IntArray, mask: IntArray) {
         for (i in first.indices) {
-            var rFirst = (first[i] and 0x00FF0000 shr 16)
-            var gFirst = (first[i] and 0x0000FF00 shr 8)
-            var bFirst = (first[i] and 0x000000FF)
+            val rFirst = (first[i] and 0x00FF0000 shr 16)
+            val gFirst = (first[i] and 0x0000FF00 shr 8)
+            val bFirst = (first[i] and 0x000000FF)
 
-            var rBlur = (blur[i] and 0x00FF0000 shr 16)
-            var gBlur = (blur[i] and 0x0000FF00 shr 8)
-            var bBlur = (blur[i] and 0x000000FF)
+            val rBlur = (blur[i] and 0x00FF0000 shr 16)
+            val gBlur = (blur[i] and 0x0000FF00 shr 8)
+            val bBlur = (blur[i] and 0x000000FF)
 
             var rMask = rFirst - rBlur
             var gMask = gFirst - gBlur
             var bMask = bFirst - bBlur
 
-            //rMask = pixelCheck(rMask)
-            //gMask = pixelCheck(gMask)
-            //bMask = pixelCheck(bMask)
-
-            if (rMask < 0) {
-                rMask = 0
-            }
-            if (gMask < 0) {
-                gMask = 0
-            }
-            if (bMask < 0) {
-                bMask = 0
-            }
-
-            if (rMask > 255) {
-                rMask = 255
-            }
-            if (gMask > 255) {
-                gMask = 255
-            }
-            if (bMask > 255) {
-                bMask = 255
-            }
+            if (rMask > 255) rMask = 255
+            if (gMask > 255) gMask = 255
+            if (bMask > 255) bMask = 255
+            if (rMask < 0) rMask = 0
+            if (gMask < 0) gMask = 0
+            if (bMask < 0) bMask = 0
 
             mask[i] = -0x1000000 or (rMask shl 16) or (gMask shl 8) or bMask
         }
@@ -236,19 +212,19 @@ class UnsharpMaskingActivity : AppCompatActivity() {
         threshold: Int
     ) {
         for (i in first.indices) {
-            var rFirst = (first[i] and 0x00FF0000 shr 16)
-            var gFirst = (first[i] and 0x0000FF00 shr 8)
-            var bFirst = (first[i] and 0x000000FF)
+            val rFirst = (first[i] and 0x00FF0000 shr 16)
+            val gFirst = (first[i] and 0x0000FF00 shr 8)
+            val bFirst = (first[i] and 0x000000FF)
 
-            var rMask = (mask[i] and 0x00FF0000 shr 16)
-            var gMask = (mask[i] and 0x0000FF00 shr 8)
-            var bMask = (mask[i] and 0x000000FF)
+            val rMask = (mask[i] and 0x00FF0000 shr 16)
+            val gMask = (mask[i] and 0x0000FF00 shr 8)
+            val bMask = (mask[i] and 0x000000FF)
 
-            var r = 0
-            var g = 0
-            var b = 0
+            var r: Int
+            var g: Int
+            var b: Int
 
-            var brightness = (0.2126 * rMask + 0.7152 * gMask + 0.0722 * bMask)
+            val brightness = (0.2126 * rMask + 0.7152 * gMask + 0.0722 * bMask)
 
             when {
                 brightness > threshold -> {
@@ -256,28 +232,12 @@ class UnsharpMaskingActivity : AppCompatActivity() {
                     g = gFirst + (amount * gMask).toInt()
                     b = bFirst + (amount * bMask).toInt()
 
-                    //r = pixelCheck(r)
-                    //g = pixelCheck(g)
-                    //b = pixelCheck(b)
-                    if (r > 255) {
-                        r = 255
-                    }
-                    if (g > 255) {
-                        g = 255
-                    }
-                    if (b > 255) {
-                        b = 255
-                    }
-
-                    if (r < 0) {
-                        r = 0
-                    }
-                    if (g < 0) {
-                        g = 0
-                    }
-                    if (b < 0) {
-                        b = 0
-                    }
+                    if (r > 255) r = 255
+                    if (g > 255) g = 255
+                    if (b > 255) b = 255
+                    if (r < 0) r = 0
+                    if (g < 0) g = 0
+                    if (b < 0) b = 0
                 }
                 else -> {
                     r = rFirst
@@ -287,13 +247,6 @@ class UnsharpMaskingActivity : AppCompatActivity() {
             }
             result[i] = -0x1000000 or (r shl 16) or (g shl 8) or b
         }
-    }
-
-    private fun pixelCheck(pixel: Int): Int {
-        var resultPixel = 0
-        if (pixel > 255) resultPixel = 255
-        if (pixel < 0) resultPixel = 0
-        return (resultPixel)
     }
 
     private fun createImageFile(): File {
